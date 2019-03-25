356e60e5ccdc963e65dd35c333819ab464834e59
veronichka124 <veronika.hromchenkova@gmail.com>
2019-03-25 11:17:09 +0200

    FCFS, SJF done

--- scripts.js
+++ scripts.js
@@ -1,47 +1,35 @@
 document.getElementById("submit").onclick = function() {submitForm()};
 document.getElementById("clear").onclick = function() {clearTable()};
-var table = document.getElementById("table").getElementsByTagName('tbody')[0];
+var table = $("#table_inputs tbody")[0];
 var animateBut = document.getElementById("animate");
 var process_array = [];
-var pressedAnimate = false;
+// var algorithm = $("#select").val();
+// 		$("#select").change(function() {	
+// 			if (algorithm == "fcfs") {
+				
+// 			} else if (algorithm  == "sjf") {
 
-//SELECT ALGORITM
-$("#select").change(function() {
-	if ($("#select").val() == "FCFS") {
-		$("#time").prop( "disabled", true )
-	} else if ($("#select").val()  == "SJF") {
-		$("#time").prop( "disabled", false )
-	}
-});
+// 			} else if (algorithm  == "sjf") {
 
+// 			}
+// 		});
 
 function submitForm() {
 	//VARIABLES
-	var process = document.getElementById("process").value;
-	var time = document.getElementById("time").value;
-	var spurta = document.getElementById("spurta").value;
+	var process = $("#process").val();
+	var arrival = $("#arrival").val();
+	var time = $("#time").val();
     var animateBut = document.getElementById("animate");
 
-    document.getElementById("process").value = '';
-	document.getElementById("time").value = '';
-	document.getElementById("spurta").value = '';
+    $("#process").val('');
+	$("#time").val('');
+	$("#arrival").val('');
 
     //FILL ARRAY
-    if (pressedAnimate == 0) {
-    	process_array.push({
-	    	process: process,
-	    	time: time
-	    });
-    } else if (pressedAnimate == 1) {
-    	process_array = [];
-    	process_array.push({
-	    	process: process,
-	    	time: time
-	    });
-	    process_array = false;
-    }
-    
-
+	process_array.push({
+    	process: process,
+    	time: time
+    });    
 
     //INSERT INTO TABLE
     var row = table.insertRow(table.rows.length);
@@ -49,43 +37,60 @@ function submitForm() {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     cell1.innerHTML = process;
-	cell2.innerHTML = time;
-	cell3.innerHTML = spurta;
+	cell2.innerHTML = arrival;
+	cell3.innerHTML = time;	
 
-	
-
-	animateButton(process_array);	
+	drawAnimation(process_array);	
 }
 
-	// SORT
-	// array.sort(function(a, b){
-	//   return a.y - b.y;
-	// });
 
-function animateButton(array) {
-	
-	var process_array = array;
-	// console.log(process_array);
-	
+function drawAnimation(array) {	
 	animateBut.onclick = function() {
-		$("#table_animation").show()
-		var count = array.length;
-		pressedAnimate = true;
-
-		array.forEach(function(item,index,array) {
-			// console.log(count); 
-			//FCFS RESULT ANIMATION
-			var row = document.getElementById("result");
-			var x = row.insertCell(array.length - count);
-			x.innerHTML = item.process;
-			count--;
-		});
+		$("#table_animation tr").empty();
+		$("#table_animation").show();
+		
+		//SELECT ALGORITM
+		var algorithm = $("#select").val();
+		// FCFS ANIMATION
+		if(algorithm == 'fcfs') {
+			fillAnimation(array);
+		}
+		// SJF ANIMATION
+		else if (algorithm == 'sjf') {
+			// SORT ARRAY
+			var sort_array = JSON.parse(JSON.stringify(array));
+			sort_array.sort(compare);
+			fillAnimation(sort_array);
+		} 
+		else if (algorithm == 'priority') {
+			console.log(algorithm);
+		}
+		else if (algorithm == 'robin') {
+			console.log(algorithm);
+		}		
 	};
 	
 }
+function compare(a, b){
+	return a.time - b.time;
+}
+
+function fillAnimation(array) {
+	var totalTime = 0;
+	var td_process = '';	
+	var td_time = '';		
+	array.forEach(function(item,index,array) {
+		td_process += '<td style="width: '+ item.time * 20 +'px;">'+ item.process +'</td>';
+		td_time += '<td style="width: '+ item.time * 20 +'px;">'+ item.time +'</td>';				
+		totalTime += parseInt(item.time);
+	});
+	$("#table_animation").html('<tr style="height: 50px; font-weight: bold;">'+td_process+'</tr>'+'<tr style="height: 20px;">'+td_time+'</tr>');
+	$("#timer").html(' <p>Total time: <strong>' + totalTime + '</strong> sec</p>');
+}
 
 function clearTable() {
-	table.remove();
+	$("#table_inputs tbody").empty();
 	$("#table_animation").hide();
 	$("#table_animation tr").empty();
+	$("#timer").empty();
 }
